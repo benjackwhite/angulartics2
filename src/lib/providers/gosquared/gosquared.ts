07a0a4cf7f15269d34884dacee6d7c21e785a809
Ben White <ben.white@netlight.com>
2019-03-22 15:35:18 +0100

    Improved detection of gosquared function

--- src/lib/providers/gosquared/gosquared.ts
+++ src/lib/providers/gosquared/gosquared.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 
 import { Angulartics2 } from 'angulartics2';
 
-declare var _gs: Function;
-
 @Injectable({ providedIn: 'root' })
 export class Angulartics2GoSquared {
   constructor(private angulartics2: Angulartics2) {
     this.angulartics2.setUserProperties.subscribe(x =>
-      this.setUserProperties(x)
+      this.setUserProperties(x),
     );
     this.angulartics2.setUserPropertiesOnce.subscribe(x =>
-      this.setUserProperties(x)
+      this.setUserProperties(x),
     );
+  }
+
+  get GoSquared(): Function {
+    return window['_gs'];
   }
 
   init(token: string) {
@@ -22,14 +24,14 @@ export class Angulartics2GoSquared {
     };
 
     const script = document.createElement('script');
-    script.innerHTML = `!function(g,s,q,r,d){r=g[r]=g[r];d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+    script.innerHTML = `!function(g,s,q,r,d){d=s.createElement(q);q=s.getElementsByTagName(q)[0];
       d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
-      insertBefore(d,q)}(window,document,'script','_gs');
-      _gs('${token}');
-      _gs('set', 'anonymizeIP', true);
-      _gs('set', 'trackLocal', true);`;
-
+      insertBefore(d,q)}(window,document,'script','_gs');`;
     document.head.appendChild(script);
+
+    this.GoSquared(token);
+    this.GoSquared('set', 'anonymizeIP', true);
+    this.GoSquared('set', 'trackLocal', true);
   }
 
   startTracking(): void {
@@ -43,7 +45,7 @@ export class Angulartics2GoSquared {
 
   pageTrack(path: string) {
     try {
-      _gs('track', path);
+      this.GoSquared('track', path);
     } catch (e) {
       if (!(e instanceof ReferenceError)) {
         throw e;
@@ -53,7 +55,7 @@ export class Angulartics2GoSquared {
 
   eventTrack(action: string, properties: any) {
     try {
-      _gs('event', action, properties);
+      this.GoSquared('event', action, properties);
     } catch (e) {
       if (!(e instanceof ReferenceError)) {
         throw e;
@@ -63,7 +65,7 @@ export class Angulartics2GoSquared {
 
   setUserProperties(properties: any) {
     try {
-      _gs('identify', properties);
+      this.GoSquared('identify', properties);
     } catch (e) {
       if (!(e instanceof ReferenceError)) {
         throw e;
